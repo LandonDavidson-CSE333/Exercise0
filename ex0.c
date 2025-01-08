d417c2700802d08850829e84d18cbaf60ae2b783
Landon Davidson <accounts@landond.tech>
2025-01-07 17:42:58 -0800

    Initial Commit

---
+++ ex0.c
@@ -0,0 +1,5 @@
+//
+// Created by landond on 1/7/25.
+//
+
+#include "ex0.h"
---
+++ ex0.h
@@ -0,0 +1,8 @@
+//
+// Created by landond on 1/7/25.
+//
+
+#ifndef EXERCISE0_EX0_H
+#define EXERCISE0_EX0_H
+
+#endif //EXERCISE0_EX0_H
