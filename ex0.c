8d7b4d4e5e59d9ce53d06bd44d626d10e038f61f
Landon Davidson <accounts@landond.tech>
2025-01-07 18:37:18 -0800

    Change: min input to 0 from 1

--- ex0.c
+++ ex0.c
@@ -11,8 +11,8 @@ int main(int argc, char** argv) {
     long int num_terms = strtol(argv[1], &end, 10);
     // if the scanner ended at the start then it didn't read any numbers and the input is invalid
     if (end == argv[1]) {printf("Argument isn't an integer\n"); return 1;}
-    // If the input is less than 1 then it is too small and the input is invalid
-    if (num_terms < 1) { printf("Input to small\n"); return 1;}
+    // If the input is less than 0 then it is too small and the input is invalid
+    if (num_terms < 0) { printf("Input to small\n"); return 1;}
     // If the input is more than 100,000 it is too big
     if (num_terms > 100000) { printf("Input to large\n"); return 1;}
     // repeatedly calculate num_terms terms of the Nilakantha series and add to pi_approx
